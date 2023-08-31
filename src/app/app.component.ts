e900827f44482f8c7ac72b8e76488f17b1b7a1b9
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-08-31 13:10:21 +0200

    Add forkJoin example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Observable, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: ''
 })
 export class AppComponent {
-  title = 'rxjs-forkjoin';
+
+  buildProfilePage() {
+    const perfilObservable: Observable<any> = apiService.getPerfil();
+    const pedidosObservable: Observable<any> = apiService.getPedidos();
+    const actividadesObservable: Observable<any> = apiService.getActividades();
+    forkJoin( [perfilObservable, pedidosObservable, actividadesObservable])
+      .subscribe(
+        ([perfil, pedidos, atividades]) => {
+          console. log( 'Perfil do usuario:', perfil);
+          console. log( 'Pedidos recientes:', pedidos);
+          console. log( 'Atividades de login:', atividades);
+          this.updateUI(perfil, pedidos, atividades);
+        },
+        error => {
+          console.log('ERROR');
+        }
+      );
+  }
 }
